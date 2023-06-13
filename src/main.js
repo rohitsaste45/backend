7b81155ed71973aabd4e18b8793ffbdc5ad2ee63
Rohit <rohitsaste45@gmail.com>
2023-06-13 19:48:37 +0530

    backend

---
+++ src/main.js
@@ -0,0 +1,40 @@
+import { MongoClient } from "mongodb";
+import express from "express";
+
+const app =express();
+
+async function addRecord(req,res){
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+
+    let db = client.db("mydb");
+    let messageCollection = db.collection("message");
+
+    let inputDoc ={
+         message : req.query.message || "default"};
+    await messageCollection.insertOne(inputDoc);
+    await client.close();
+
+    console.log("Record Added");
+    //res.send("Record Added");
+    res.json({opr:"success"})
+}
+
+async function findAllMessage(req,res){
+    const uri = "mongodb://127.0.0.1:27017";
+    const client = new MongoClient(uri);
+
+    let db = client.db("mydb");
+    let messageCollection = db.collection("message");
+    let list = await messageCollection.find().toArray();
+    
+    await client.close();
+    res.json(list)
+}
+
+// http://localhost:4000/addRecord
+app.get("/addRecord",addRecord)
+// http://localhost:4000/findAllMessage
+app.get("/findAllMessage",findAllMessage)
+// http://localhost:4000/
+app.listen(4000)
